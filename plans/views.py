9fbb815aeac7731770e459d67ab816f12c0f704e
brahim024 <ifninos168@gmail.com>
2021-01-22 18:04:58 +0100

    add charge default and add selected options

--- plans/views.py
+++ plans/views.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from django.http import JsonResponse
 import stripe
 # Create your views here.
+stripe.api_key = 'sk_test_51IC2qyA1yapRcmMMe2xQJq1Tky7mdaZQiyLUo45pIvka9lZQkMD9TSq2Ej3eaI61E1x7Rl6cnLEaZXSMzjNNcPrr00XxgvaHuT'
 
 def index(request):
 	return render(request, 'index.html')
@@ -12,6 +13,17 @@ def charge(request):
 	amount = 5
 	if request.method == 'POST':
 		print('Data:', request.POST)
+		customer=stripe.Customer.create(
+			email=request.POST['email'],
+			name=request.POST['nickname'],
+			source=request.POST['stripeToken']
+		)
+		charge=stripe.Charge.create(
+			customer=customer,
+			amount=500,
+			currency="usd",
+			description="Donation"
+			)
 
 	return redirect(reverse('success', args=[amount]))
 
