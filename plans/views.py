af2bdb2620fa030c4fbd18b73f0f75cc886a1665
brahim024 <ifninos168@gmail.com>
2021-01-22 15:54:08 +0100

    start donation app

---
+++ plans/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.http import JsonResponse
+import stripe
+# Create your views here.
+
+def index(request):
+	return render(request, 'index.html')
+
+
+def charge(request):
+	amount = 5
+	if request.method == 'POST':
+		print('Data:', request.POST)
+
+	return redirect(reverse('success', args=[amount]))
+
+
+def successMsg(request, args):
+	amount = args
+	return render(request, 'success.html', {'amount':amount})
